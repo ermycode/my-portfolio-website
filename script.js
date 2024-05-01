0cdc50a4b0f17b10a1b90041102ff5c9ed9c33e4
Ermias Amualto <ermyblue21@gmail.com>
2024-05-02 00:42:36 +0300

    updated the text message of automatic reply

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function myFunction() {
   alert("Great!")
-  document.getElementById("demo").innerHTML = "Ermias, Paragraph changed.";
+  document.getElementById("demo").innerHTML = "Thank you for getting in touch! We've received your message and will make sure to respond within the next two business days. Your inquiry is valuable to us, and we appreciate your patience. Should you need immediate assistance, please don't hesitate to reach out to us directly at 404 643 4450. We look forward to assisting you!.";
 }
